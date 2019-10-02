edeecdbc8746a68ca14e659927178adffb63550b
qiaoleyu <1026573472@qq.com>
2019-10-02 09:14:43 +0800

    5

--- src/components/index.vue
+++ src/components/index.vue
@@ -204,13 +204,38 @@
       <el-col :span="6"><div class="grid-content bg-purple" style="height: 80px;line-height: 80px">好</div></el-col>
       <el-col :span="6"><div class="grid-content bg-purple" style="height: 80px;line-height: 80px">省</div></el-col>
     </el-row>
+    <el-row>
+      <el-col :span="3">
+        <div class="grid-content bg-purple" style="height: 200px;line-height: 50px">
+          帮助中心
+        </div>
+      </el-col>
+      <el-col :span="3"><div class="grid-content bg-purple" style="height: 200px;line-height: 50px">服务支持</div></el-col>
+      <el-col :span="3"><div class="grid-content bg-purple" style="height:200px;line-height: 50px">线下门店</div></el-col>
+      <el-col :span="3"><div class="grid-content bg-purple" style="height: 200px;line-height: 50px">关于小米</div></el-col>
+      <el-col :span="3"><div class="grid-content bg-purple" style="height: 200px;line-height: 50px">关于我们</div></el-col>
+      <el-col :span="3"><div class="grid-content bg-purple" style="height: 200px;line-height: 50px">特色服务</div></el-col>
+      <el-col :span="6"><div class="grid-content bg-purple" style="height: 200px;line-height: 200px">人工客服</div></el-col>
+    </el-row>
     <!--bottom-->
     <el-row>
         <el-col :span="24">
-          <div class="grid-content bg-purple" style="height: 100px;line-height: 50px">
+          <div class="grid-content bg-purple" style="height: 100px;line-height: 30px;font-size: 12px;text-align: center">
             <ul>
-              <li >保障</li><br>
-              <li>版权</li>
+              <li >
+                小米商城 | MIUI | 米家 | 米聊 | 多看 | 游戏 |路由器 | 米粉卡 | 政企服务 | 小米天猫店 | 小米集团隐私政策 | 小米公司儿童信息保护规则 | 小米商城隐私政策 | 小米商城用户协议 | 问题反馈 | 廉正举报 | 诚信合规 | Select Location</li><br>
+              <li>
+                © mi.com 京ICP证110507号 京ICP备10046444号 京公网安备11010802020134号 京网文[2017]1530-131号
+              </li>
+              <li>
+                （京）网械平台备字（2018）第00005号 互联网药品信息服务资格证 (京) -非经营性-2014-0090 营业执照 医疗器械公告
+              </li>
+              <li>
+                增值电信业务许可证  网络食品经营备案（京）【2018】WLSPJYBAHF-12   食品经营许可证
+              </li>
+              <li>
+                违法和不良信息举报电话：185-0130-1238 知识产权侵权投诉 本网站所列数据，除特殊说明，所有数据均出自我司实验室测试
+              </li>
             </ul>
           </div>
         </el-col>
