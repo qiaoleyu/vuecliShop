ada343c38be534402e5e589182d22db4b1999fa2
qiaoleyu <1026573472@qq.com>
2019-10-10 11:58:39 +0800

    11

--- src/components/shopDetial.vue
+++ src/components/shopDetial.vue
@@ -28,7 +28,7 @@
               &nbsp;&nbsp;&nbsp;
               <router-link type="info" :to="{name:'userRegist'}" style="color:black"><a>免费注册</a></router-link>
               &nbsp;&nbsp;&nbsp;
-              <span type="info" style="color:black"><a @click="logout()">退出</a></span>
+              <span type="info" style="color:black;cursor: pointer"><a @click="logout()">退出</a></span>
             </div>
           </el-col>
           <el-col :span="14">
