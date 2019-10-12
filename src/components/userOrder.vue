8f197ad172a93c54ca413d59b78410c0a96f1d9f
candy-hub <1340744230@qq.com>
2019-10-12 17:34:25 +0800

    candy commit

--- src/components/userOrder.vue
+++ src/components/userOrder.vue
@@ -70,9 +70,6 @@
       </el-table-column>
     </el-table>
     <div style="background-color: aliceblue; height: 80px;margin: auto;margin-top: 20px">
-
-      <div style="float: right;margin-top: 20px;margin-right: 60px"><el-button type="warning" plain style="width: 100px" @click="reback()">取消</el-button></div>
-
         <div style="float: right;margin-top: 20px;margin-right: 30px"><el-button v-if="this.statue==2" type="success" plain style="width: 100px" @click="pay()">立即支付</el-button></div>
 
         <div style="float: right;margin-top: 20px;margin-right: 30px"><el-button type="primary" plain style="width: 100px" @click="cart()">上一步</el-button></div>
@@ -239,14 +236,7 @@
           })
         }
 
-      },
-      reback:function () {
-        axios.post("api/delOrder",this.order).then(res=>{
-            if(res.data.code==1){
-                this.$router.push("/userCart");
-            }
-        })
-      },
+      }
     }
   }
 
